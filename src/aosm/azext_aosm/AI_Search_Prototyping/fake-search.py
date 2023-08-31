0afe80caa09a50aa904ed54e275eab5e8e425f68
Nikita <t-nipatel@microsoft.com>
2023-08-31 09:26:09 +0100

    Detailed anonymised network functions added to fake search.

--- src/aosm/azext_aosm/AI_Search_Prototyping/fake-search.py
+++ src/aosm/azext_aosm/AI_Search_Prototyping/fake-search.py
@@ -2,7 +2,7 @@ import asyncio
 from typing import Tuple
 
 import semantic_kernel as sk
-from semantic_kernel.connectors.ai.open_ai import AzureTextEmbedding, AzureTextCompletion
+from semantic_kernel.connectors.ai.open_ai import AzureTextEmbedding, AzureTextCompletion, AzureChatCompletion
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
@@ -11,21 +11,81 @@ client = SecretClient(vault_url="https://azopenai-dev-kv.vault.azure.net/", cred
 ai_key = client.get_secret("AIKey1")
 
 async def fake_repo(kernel: sk.Kernel) -> None:
-    await kernel.memory.save_information_async("fake-repo", id="nf1", text="https://hub.docker.com/_/nginx")
-    await kernel.memory.save_information_async("fake-repo", id="nf2", text="https://hub.docker.com/_/haproxy")
-    await kernel.memory.save_information_async("fake-repo", id="nf3", text="https://hub.docker.com/r/ubuntu/squid")
-    await kernel.memory.save_information_async("fake-repo", id="nf4", text="https://hub.docker.com/_/traefik")
-    await kernel.memory.save_information_async("fake-repo", id="nf5", text="https://hub.docker.com/_/consul")
-    await kernel.memory.save_information_async("fake-repo", id="dummy", text="Dummy place filler")
+    await kernel.memory.save_information_async("fake-repo", id="nf1", text="""
+nf1 is a powerful network function that has the following features:
+Ability to handle more than 10,000 simultaneous connections with a low memory footprint (~2.5 MB per 10k inactive HTTP keep-alive connections). 
+Handling of static files, index files and auto-indexing
+Reverse proxy with caching
+Load balancing[26] with in-band health checks
+TLS/SSL with SNI and OCSP stapling support, via OpenSSL
+FastCGI, SCGI, uWSGI support with caching
+gRPC support since March 2018, version 1.13.10.
+Name- and IP address-based virtual servers
+IPv6-compatible
+WebSockets since 1.3.13, including acting as a reverse proxy and do load balancing of WebSocket applications.
+HTTP/1.1 Upgrade (101 Switching Protocols)
+HTTP/2 protocol support
+HTTP/3 protocol support (experimental since 1.25.0)
+URL rewriting and redirection
+TLS/SSL support
+STARTTLS support
+SMTP, POP3, and IMAP proxy
+Requires authentication using an external HTTP server or by an authentication script
+Other features include upgrading executable and configuration without client connections loss, and a module-based architecture with both core and third-party module support.""")
+    await kernel.memory.save_information_async("fake-repo", id="nf2", text="""
+nf2 is a powerful network function that supports a variety of features, many implemented as compiled modules which extend the core functionality. These can range from authentication schemes to supporting server-side programming languages such as Perl, Python, Tcl and PHP. Popular authentication modules include mod_access, mod_auth, mod_digest, and mod_auth_digest, the successor to mod_digest. A sample of other features include Secure Sockets Layer and Transport Layer Security support (mod_ssl), a proxy module (mod_proxy), a URL rewriting module (mod_rewrite), custom log files (mod_log_config), and filtering support (mod_include and mod_ext_filter).
+Popular compression methods on nf2 include the external extension module, mod_gzip, implemented to help with reduction of the size (weight) of web pages served over HTTP. ModSecurity is an open source intrusion detection and prevention engine for Web applications. nf2 logs can be analyzed through a Web browser using free scripts, such as AWStats/W3Perl or Visitors.
+Virtual hosting allows one nf2 installation to serve many different websites. For example, one computer with one nf2 installation could simultaneously serve example.com, example.org, test47.test-server.example.edu, etc.
+nf2 features configurable error messages, DBMS-based authentication databases, content negotiation and supports several graphical user interfaces (GUIs).
+It supports password authentication and digital certificate authentication. Because the source code is freely available, anyone can adapt the server for specific needs, and there is a large public library of nf2 add-ons.
+A more detailed list of features is provided below:
+Loadable Dynamic Modules
+Multiple Request Processing modes (MPMs) including Event-based/Async, Threaded and Prefork.
+Highly scalable (easily handles more than 10,000 simultaneous connections)
+Handling of static files, index files, auto-indexing and content negotiation
+.htaccess per-directory configuration support
+Reverse proxy with caching
+Load balancing with in-band health checks
+Multiple load balancing mechanisms
+Fault tolerance and Failover with automatic recovery
+WebSocket, FastCGI, SCGI, AJP and uWSGI support with caching
+Dynamic configuration
+TLS/SSL with SNI and OCSP stapling support, via OpenSSL or wolfSSL.
+Name- and IP address-based virtual servers
+IPv6-compatible
+HTTP/2 support
+Fine-grained authentication and authorization access control
+gzip compression and decompression
+URL rewriting
+Headers and content rewriting
+Custom logging with rotation
+Concurrent connection limiting
+Request processing rate limiting
+Bandwidth throttling
+Server Side Includes
+IP address-based geolocation
+User and Session tracking
+WebDAV
+Embedded Perl, PHP and Lua scripting
+CGI support
+public_html per-user web-pages
+Generic expression parser
+Real-time status views
+FTP support (by a separate module)
+""")
+    await kernel.memory.save_information_async("fake-repo", id="nf3", text="nf3 is not a real network function and couldn't be used in a network service design.")
+    await kernel.memory.save_information_async("fake-repo", id="nf4", text="nf4 is a retired network function.")
+    await kernel.memory.save_information_async("fake-repo", id="nf5", text="nf5 is a highly unrealiable network function that is being retired next Monday.")
+    await kernel.memory.save_information_async("fake-repo", id="nf6", text="nf6 is not a network function.")
 
 async def examples(kernel) -> None:
     questions = [
-        "What is nf1?"
-        "What is nf2?"
-        "What is nf3?"
-        "What is nf4?"
-        "What is nf5?"
-        "What is dummy?"
+        "What is nf1?",
+        "What is nf2?",
+        "What is nf3?",
+        "What is nf4?",
+        "What is nf5?",
+        "What is nf6?"
     ]
 
     for question in questions:
@@ -63,7 +123,7 @@ async def completion_api(
     context["fact3"] = "What is nf3?"
     context["fact4"] = "What is nf4?"
     context["fact5"] = "What is nf5?"
-    context["fact6"] = "What is dummy?"
+    context["fact6"] = "What is nf6?"
 
     context[sk.core_skills.TextMemorySkill.COLLECTION_PARAM] = "fake-repo"
     context[sk.core_skills.TextMemorySkill.RELEVANCE_PARAM] = 0.4
@@ -96,15 +156,18 @@ async def chat(
 
 async def main() -> None:
     kernel = sk.Kernel()
-    kernel.add_text_completion_service(
-        "completion", AzureTextCompletion("text-davinci-003-aiops", "https://azopenai-aiops.openai.azure.com/", ai_key.value)
+    #kernel.add_text_completion_service(
+    #    "completion", AzureTextCompletion("text-davinci-003-aiops", "https://azopenai-aiops.openai.azure.com/", ai_key.value)
+    #)
+
+    kernel.add_chat_service(
+        "chat", AzureChatCompletion("gpt35depl1", "https://azopenai-aiops.openai.azure.com/", ai_key.value)
     )
     kernel.add_text_embedding_generation_service(
         "embedding", AzureTextEmbedding("text-embedding-ada-002", "https://azopenai-aiops.openai.azure.com/", ai_key.value)
     )
     kernel.register_memory_store(memory_store=sk.memory.VolatileMemoryStore())
     kernel.import_skill(sk.core_skills.TextMemorySkill())
-    
     await fake_repo(kernel)
 
     await examples(kernel)
