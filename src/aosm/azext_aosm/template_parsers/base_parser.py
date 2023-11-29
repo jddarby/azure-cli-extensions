39d24b57f3093e16055e30f27ec1a81be0ff9d5c
Andy Churchard <andy.churchard@metaswitch.com>
2023-11-29 10:04:21 +0000

    Add parameters to BaseParser

--- src/aosm/azext_aosm/template_parsers/base_parser.py
+++ src/aosm/azext_aosm/template_parsers/base_parser.py
@@ -1,6 +1,13 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from pathlib import Path
 
+@dataclass
 class BaseParser(ABC):
+
+    template_path: Path
+    defaults_path: Path
+
     @abstractmethod
     def get_defaults(self):
         pass
