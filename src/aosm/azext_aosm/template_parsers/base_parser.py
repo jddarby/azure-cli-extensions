0d78d45f963c5a7d1db901f09d7370d4c5151dac
Andy Churchard <andy.churchard@metaswitch.com>
2023-11-29 10:43:35 +0000

    Make defaults_path optional in base_parser

--- src/aosm/azext_aosm/template_parsers/base_parser.py
+++ src/aosm/azext_aosm/template_parsers/base_parser.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional
 
 @dataclass
 class BaseParser(ABC):
 
     template_path: Path
-    defaults_path: Path
+    defaults_path: Optional(Path)
 
     @abstractmethod
     def get_defaults(self):
