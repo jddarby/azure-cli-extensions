366812a8534180232be49c3cec9e2a15e3bd4abd
Jordan <jordan.layton@metaswitch.com>
2023-12-09 10:30:25 +0000

    fixed cnf bicep template

--- src/aosm/azext_aosm/cli_handlers/onboarding_cnf_handler.py
+++ src/aosm/azext_aosm/cli_handlers/onboarding_cnf_handler.py
@@ -9,7 +9,10 @@ from azext_aosm.configuration_models.onboarding_cnf_input_config import (
 )
 from azext_aosm.build_processors.helm_chart_processor import HelmChartProcessor
 from .onboarding_nfd_base_handler import OnboardingNFDBaseCLIHandler
-from azext_aosm.vendored_sdks.models import ManifestArtifactFormat
+from azext_aosm.vendored_sdks.models import (
+    ManifestArtifactFormat, AzureArcKubernetesHelmApplication, 
+    AzureArcKubernetesArtifactProfile, HelmArtifactProfile,
+    AzureArcKubernetesDeployMappingRuleProfile, HelmMappingRuleProfile, HelmMappingRuleProfileOptions)
 # from azext_aosm.vendored_sdks.models import ArtifactStore
 
 class OnboardingCNFCLIHandler(OnboardingNFDBaseCLIHandler):
@@ -49,6 +52,7 @@ class OnboardingCNFCLIHandler(OnboardingNFDBaseCLIHandler):
 
         template_path = self._get_template_path("cnfartifactmanifest.bicep.j2")
         bicep_contents = self._write_manifest_bicep_file(template_path, artifact_list)
+        print("JORDAN MANIFEST", bicep_contents)
 
         return bicep_contents
 
@@ -61,15 +65,38 @@ class OnboardingCNFCLIHandler(OnboardingNFDBaseCLIHandler):
         """Build the resource bicep file."""
         # TODO: Implement
         nf_application_list = []
-        for helm_package in self.config.helm_packages:
-            processed_helm = HelmChartProcessor(
-                helm_package.name,
-                self.config.acr_artifact_store_name,
-                helm_package,
-            )
-            nf_application_list.append(processed_helm.generate_nf_application())
+        # for helm_package in self.config.helm_packages:
+        #     processed_helm = HelmChartProcessor(
+        #         helm_package.name,
+        #         self.config.acr_artifact_store_name,
+        #         helm_package,
+        #     )
+        #     nf_application_list.append(processed_helm.generate_nf_application())
         
+        # Jordan: mocked nf applicaton
+        test_nf_application = AzureArcKubernetesHelmApplication(
+            name="testNFApplication",
+            depends_on_profile=[],
+            artifact_profile=AzureArcKubernetesArtifactProfile(artifact_store="testArtifactStore", 
+                                                               helm_artifact_profile=HelmArtifactProfile(
+                                                                   helm_package_name="testHelmPackage",
+                                                                   helm_package_version_range="1.0.0",
+                                                                   registry_values_paths=["testPath1", "testPath2"],
+                                                                   image_pull_secrets_values_paths=["testPath3", "testPath4"])),
+            deploy_parameters_mapping_rule_profile=AzureArcKubernetesDeployMappingRuleProfile(application_enablement="testApplicationEnablement",
+                                                                                              helm_mapping_rule_profile=HelmMappingRuleProfile(
+                                                                                                  release_namespace="testReleaseNamespace",
+                                                                                                  release_name="testReleaseName",
+                                                                                                  helm_package_version="1.0.0",
+                                                                                                  values="testValues",
+                                                                                                  options=None
+                                                                                              )))
+        nf_application_list.append(test_nf_application)
         template_path = self._get_template_path("cnfdefinition.bicep.j2")
-        bicep_contents = self._write_definition_bicep_file(template_path, nf_application_list)
         
+        #   values: string(loadJsonContent('configMappings/{{ configuration.valueMappingsFile }}'))
+           
+        bicep_contents = self._write_definition_bicep_file(template_path, nf_application_list)
+        print("JORDAN NF", bicep_contents)
+
         return bicep_contents
